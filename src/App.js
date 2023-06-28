1eda3d52b9f1f707122f2dd4fda9a867e6bffa6a
sahilpayla <1shraddhanand@gmail.com>
2023-06-28 10:13:24 +0530

    working on side navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
 import Cars from './components/cars/Cars';
 import DropDown from './components/dropdown/DropDown';
 import { useState } from 'react';
 import Header from './components/header/Header';
 import Flip from './components/flip/Flip';
 import Slider from './components/slider/Slider';
+import Navbar from './components/sideNavbar/Navbar';
 
 function App() {
   const [selected, setSelected] = useState("Lang");
@@ -16,7 +16,8 @@ function App() {
       {/* <DropDown selected={selected} setSelected={setSelected} /> */}
       {/* <Header /> */}
       {/* <Flip/> */}
-      <Slider />
+      {/* <Slider /> */}
+      <Navbar />
     </>
   );
 }
---
+++ src/components/sideNavbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <>
+        <h1>Side Navbar</h1>
+    </>
+  )
+}
+
+export default Navbar
