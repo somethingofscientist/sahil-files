e79995160e16ea07ac84435726fad1fb144d7174
sahilpayla <1shraddhanand@gmail.com>
2023-08-01 19:05:30 +0530

    sahil ->  removing error

--- src/components/infiniteScroller/InfiniteScroller.js
+++ src/components/infiniteScroller/InfiniteScroller.js
@@ -1,13 +1,21 @@
 import styles from './InfinteScroller.module.css';
 import React, { useEffect, useRef, useState } from 'react';
 import Slider from 'infinite-react-carousel';
-import AutoplaySlider from 'react-awesome-slider/hoc/autoplay';
-import AwesomeSliderStyles from 'react-awesome-slider/src/styled/fold-out-animation.scss';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 
 const InfiniteScroller = () => {
     return (
         <>
             <h1>Carousle Awesome</h1>
+            <AwesomeSlider>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+            </AwesomeSlider>
+
+
         </>
     );
 }
